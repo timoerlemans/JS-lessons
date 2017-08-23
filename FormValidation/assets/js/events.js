ab853a8c720f732d54c2ab5aa16264c6df226204
Tim Oerlemans <timoerlemans@gmail.com>
2017-08-23 21:54:17 +0200

    Refactored JS a bit

--- FormValidation/assets/js/events.js
+++ FormValidation/assets/js/events.js
@@ -1,24 +1,19 @@
 // Check if document is ready
-document.addEventListener('DOMContentLoaded', function () {
-    init();
-}, false);
-
+document.addEventListener('DOMContentLoaded', init, false);
 
 // Run this function when document is ready
 function init() {
-    var inputAll = document.querySelectorAll('input:not(#submit)'),
-        formError = document.querySelector('.form__error');
-
-
     // Add event listener on submit button
-    document.querySelector("#submit").addEventListener('click', validateForm);
+    document.querySelector('form').addEventListener('submit', validateForm);
 }
 
 // This function validates the input fields on the forms
-function validateForm() {
-    var invalid = false,
+function validateForm(event) {
+    var valid = true,
         regEx,
-        correct;
+        correct,
+        inputAll = document.querySelectorAll('input:not(#submit)'),
+        formError = document.querySelector('.form__error');
 
     // Prevent default behaviour (posting the form), because we want to do this via AJAX.
     event.preventDefault();
@@ -28,59 +23,60 @@ function validateForm() {
 
     // Loop through all input fields
     for (i = 0; i < inputAll.length; i++) {
-        var elInvalid = false;
+        var el = inputAll[i];
+        var elValid = true;
 
         // Check if input field is empty
-        if (!inputAll[i].value) {
-            invalid = true;
-            elInvalid = true;
-            formError.innerHTML += '<li>' + inputAll[i].className + ' is empty!' + '</li>';
+        if (!el.value) {
+            valid = false;
+            elValid = false;
+            formError.innerHTML += '<li>' + el.className + ' is empty!' + '</li>';
         }
 
 
         // Check if input field is not empty and if input field type is email, then check if value is a valid email address
-        if (inputAll[i].type === 'email' && !elInvalid) {
+        if (el.type === 'email' && valid) {
             regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            correct = regEx.test(inputAll[i].value);
+            correct = regEx.test(el.value);
             if (!correct) {
-                invalid = true;
-                elInvalid = true;
-                formError.innerHTML += '<li>' + inputAll[i].className + ' is not a valid email address!' + '</li>';
+                valid = false;
+                elValid = false;
+                formError.innerHTML += '<li>' + el.className + ' is not a valid email address!' + '</li>';
             }
         }
 
         // Check if input field is not empty and if input field type is email, then check if value is a valid Dutch phonenumber
-        if (inputAll[i].type === 'tel' && !elInvalid) {
+        if (el.type === 'tel' && valid) {
             regEx = /^((((0031)|(\+31))(\-)?6(\-)?[0-9]{8})|(06(\-)?[0-9]{8})|(((0031)|(\+31))(\-)?[1-9]{1}(([0-9](\-)?[0-9]{7})|([0-9]{2}(\-)?[0-9]{6})))|([0]{1}[1-9]{1}(([0-9](\-)?[0-9]{7})|([0-9]{2}(\-)?[0-9]{6}))))$/;
-            correct = regEx.test(inputAll[i].value);
+            correct = regEx.test(el.value);
             if (!correct) {
-                invalid = true;
-                elInvalid = true;
-                formError.innerHTML += '<li>' + inputAll[i].className + ' is not a valid phone number!' + '</li>';
+                valid = false;
+                elValid = false;
+                formError.innerHTML += '<li>' + el.className + ' is not a valid phone number!' + '</li>';
             }
         }
 
         // Check if input field is not empty and if input field type is email, then check if value meets all the password requirements
-        if (inputAll[i].type === 'password' && !elInvalid) {
+        if (el.type === 'password' && valid) {
             regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
-            correct = regEx.test(inputAll[i].value);
+            correct = regEx.test(el.value);
             if (!correct) {
-                invalid = true;
-                elInvalid = true;
+                valid = false;
+                elValid = false;
                 formError.innerHTML += '<li>' + 'Password should be at least 8 characters long and should at least contain  1 uppercase letter, 1 lowercase letter, 1 number and 1 special character' + '</li>';
             }
         }
 
-        // if input field is not validated, make border red, otherwise make it green
-        if (!elInvalid) {
-            inputAll[i].style.borderColor = '#ccc';
+        // if input field is not validated, make border red, otherwise make it neutral
+        if (elValid) {
+            el.classList.remove('error');
         } else {
-            inputAll[i].style.borderColor = 'red';
+            el.classList.add('error');
         }
     }
-
+    
     // when whole form is validated, run this function
-    if (!invalid) {
+    if (valid) {
         registerUser();
     }
 }
--- FormValidation/index.php
+++ FormValidation/index.php
@@ -9,10 +9,10 @@
 <section class="form__wrapper">
 	<form action="/index.php" class="form">
 		<ul class="form__error"></ul>
-		<label>Name: <input type="text" class="name"></label>
-		<label>Email: <input type="email" class="email"></label>
-		<label>Password: <input type="password" class="password"></label>
-		<label>Phone:<input type="tel" class="phonenumber"></label>
+		<label>Name: <input type="text" required class="name"></label>
+		<label>Email: <input type="email" required class="email"></label>
+		<label>Password: <input type="password" required class="password"></label>
+		<label>Phone:<input type="tel" required class="phonenumber"></label>
 		<input type="submit" role="button" value="Opslaan" id="submit">
 	</form>
 </section>
