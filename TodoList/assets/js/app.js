e9a58778e6420059834f77227d6037d489076bf0
Tim Oerlemans <timoerlemans@gmail.com>
2018-01-27 22:54:18 +0100

    Ability to add tasks and complete them

--- TodoList/assets/js/app.js
+++ TodoList/assets/js/app.js
@@ -10,14 +10,48 @@ $(function() {
 
   $('.js-add-item').on('submit', addTask);
 
+  $('body').on('change', '.checkbox', function(e) {
+    e.preventDefault();
+    var parentEl = $(this)
+      .parent()
+      .parent();
+    console.log(parentEl);
+    if ($(this).is(':checked')) {
+      bool = true;
+    } else {
+      bool = false;
+    }
+    completeTask(parentEl, bool);
+  });
+
   function addTask(e) {
     e.preventDefault();
     var taskInputVal = taskInput.val();
 
-    todoList.prepend('<li class="todo-list__item">' + taskInputVal + '</li>');
+    todoList.prepend(appendTask(taskInputVal, 'todo-list__item'));
 
     if (defaultListItem.length > 0) {
       defaultListItem.remove();
+    }
+  }
+
+  function appendTask(val, itemClass) {
+    return (
+      '<li class="' +
+      itemClass +
+      '"><label><input type="checkbox" class="checkbox">' +
+      val +
+      '</label></li>'
+    );
+  }
+
+  function completeTask(el, bool) {
+    var selectedEl = el;
+    el.remove();
+    if (bool) {
+      $('.js-completed-list').append(el);
+    } else {
+      $('.js-todo-list').append(el);
     }
   }
 });
